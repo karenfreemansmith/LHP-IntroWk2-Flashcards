ddd1135ee84ba496ced488a011831cf64cb4f10e
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2016-03-29 15:08:31 -0700

    first version of flashcards - 1 page of web development terms

---
+++ js/scripts.js
@@ -0,0 +1,63 @@
+$(document).ready(function() {
+  //alert("jQuery loaded!"); //test to see if jQuery was set up right
+  $("p.a").click(function() {
+    $("p.a").toggle();
+  });
+  $("p.b").click(function() {
+    $("p.b").toggle();
+  });
+  $("p.c").click(function() {
+    $("p.c").toggle();
+  });
+  $("p.d").click(function() {
+    $("p.d").toggle();
+  });
+  $("p.e").click(function() {
+    $("p.e").toggle();
+  });
+  $("p.f").click(function() {
+    $("p.f").toggle();
+  });
+  $("p.g").click(function() {
+    $("p.g").toggle();
+  });
+  $("p.h").click(function() {
+    $("p.h").toggle();
+  });
+  $("p.i").click(function() {
+    $("p.i").toggle();
+  });
+  $("p.j").click(function() {
+    $("p.j").toggle();
+  });
+  $("p.k").click(function() {
+    $("p.k").toggle();
+  });
+  $("p.l").click(function() {
+    $("p.l").toggle();
+  });
+  $("p.m").click(function() {
+    $("p.m").toggle();
+  });
+  $("p.n").click(function() {
+    $("p.n").toggle();
+  });
+  $("p.o").click(function() {
+    $("p.o").toggle();
+  });
+  $("p.p").click(function() {
+    $("p.p").toggle();
+  });
+  $("p.q").click(function() {
+    $("p.q").toggle();
+  });
+  $("p.r").click(function() {
+    $("p.r").toggle();
+  });
+  $("p.s").click(function() {
+    $("p.s").toggle();
+  });
+  $("p.t").click(function() {
+    $("p.t").toggle();
+  });
+});
